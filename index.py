7ae0e6574c2db383dbb2dd0a09eaf737e4c4b626
Elderguard <111159021+Elderguard@users.noreply.github.com>
2024-04-20 11:46:57 -0300

    api routes updated, about page image fixed and other route tests

--- index.py
+++ index.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template('hobbies.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
 def main():
     app.run()
 
